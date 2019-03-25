af6f0b4923752f3c4a098dfceae2d7b471075a60
wuhan <wuhan5396@outlook.com>
2019-03-25 11:17:01 +0800

    5

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -3,7 +3,6 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
@@ -12,4 +11,18 @@ new Vue({
   el: '#app',
   router,
   render: h => h(App)
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    sessionStorage.removeItem('user')
+  }
+  var user = sessionStorage.getItem('user')
+  if (!user && to.path !== '/login') {
+    next({
+      path: '/login'
+    })
+  } else {
+    next()
+  }
 })
