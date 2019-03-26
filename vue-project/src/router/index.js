28bb8af37cb1165916f289d3abfc1507203576af
wuhan <wuhan5396@outlook.com>
2019-03-26 10:04:35 +0800

    4

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -2,15 +2,23 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Container from '@/container/Container'
 import Login from '@/views/TheLogin'
-
+import Dashboard from '@/views/dashboard'
+import Article from '@/views/article'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/dashboard',
       name: 'Container',
-      component: Container
+      component: Container,
+      children: [
+        // eslint-disable-next-line standard/object-curly-even-spacing
+        {path: 'dashboard', name: '首页', component: Dashboard },
+        // eslint-disable-next-line standard/object-curly-even-spacing
+        {path: 'article', name: '文章', component: Article }
+      ]
     },
     {
       path: '/login',
